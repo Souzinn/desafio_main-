a076fd935940cee3d0fbd1650d57d9e197af98ef
Souza <bossviti064@gmail.com>
2025-04-07 13:18:59 -0300

    ✨ feat: create Home basic view for listing Pokémons

--- src/views/pokemons.vue
+++ src/views/pokemons.vue
@@ -1,1 +1,37 @@
-<template><H1>ola</H1></template>
+<script setup>
+import cardemphasisvue from "../components/abstractions/cardEmphasis.vue";
+import cardPokemon from "../components/pokemon/cardPokemon.vue";
+import copy from "../components/abstractions/copyText.vue";
+import inputSearch from "../components/ui/input.vue";
+</script>
+
+<template>
+  <div class="content p-4">
+    <div>
+      <copy />
+    </div>
+
+    <div class="container p-4">
+      <h5 class="mb-4">Mais votados</h5>
+      <div class="d-flex justify-content-between flex-wrap gap-3">
+        <cardemphasisvue class="border rounded p-3 bg-dark-subtle text-white" />
+      </div>
+    </div>
+
+    <div class="container p-4">
+      <div>
+        <inputSearch />
+      </div>
+    </div>
+
+    <div class="container p-4">
+      <div>
+        <inputSearch />
+      </div>
+
+      <div class="d-flex flex-column gap-3 my-4">
+        <cardPokemon class="border rounded p-3 bg-dark-subtle text-white" />
+      </div>
+    </div>
+  </div>
+</template>
