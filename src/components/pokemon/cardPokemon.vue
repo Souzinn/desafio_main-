f693492ec708815ce90c3df7b41d1f8a7baafe44
Souza <bossviti064@gmail.com>
2025-04-06 21:27:23 -0300

    ✨ feat: initial template for CardPokemon component

---
+++ src/components/pokemon/cardPokemon.vue
@@ -0,0 +1,21 @@
+<script></script>
+
+<template>
+  <div
+    class="d-flex align-items-center justify-content-between p-3 border rounded shadow-sm bg-white mb-3"
+  >
+    <div class="d-flex align-items-center gap-3">
+      <img src="" alt="" class="img-fluid" style="width: 64px; height: 64px" />
+      <div>
+        <span class="text-muted small">#001</span>
+        <p class="mb-0 fw-semibold text-capitalize">nome</p>
+      </div>
+    </div>
+
+    <div>
+      <span class="badge bg-primary text-capitalize">type</span>
+    </div>
+
+    <button class="btn btn-outline-primary ms-3">more</button>
+  </div>
+</template>
