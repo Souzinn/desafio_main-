dc4244b3e2e77e15af84ddae804884f68709d7bb
Souza <bossviti064@gmail.com>
2025-04-08 10:17:42 -0300

    ♻️ refactor: update service to accept offset from store

--- src/service/index.js
+++ src/service/index.js
@@ -4,15 +4,14 @@ class requestdata {
   async fetchReposity(page) {
     try {
       const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`
+        `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=30`
       );
-      console.log(response.data);
+      const data = response.data;
+      return data;
     } catch (err) {
       console.error(err);
     }
   }
 }
 
-const request = new requestdata();
-request.fetchReposity(2); //test
 export default requestdata;
