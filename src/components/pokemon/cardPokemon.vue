a6cd5c87d91f72917349b8f40e33a9ea7438a3de
Souza <bossviti064@gmail.com>
2025-04-08 10:15:01 -0300

    ✨ feat: fetch Pokémon data on mounted()

--- src/components/pokemon/cardPokemon.vue
+++ src/components/pokemon/cardPokemon.vue
@@ -1,21 +1,32 @@
-<script></script>
+<script>
+export default {
+  props: {
+    pokemon: Object,
+  },
+};
+</script>
 
 <template>
   <div
     class="d-flex align-items-center justify-content-between p-3 border rounded shadow-sm bg-white mb-3"
   >
     <div class="d-flex align-items-center gap-3">
-      <img src="" alt="" class="img-fluid" style="width: 64px; height: 64px" />
+      <img
+        :src="`https://img.pokemondb.net/sprites/scarlet-violet/icon/${pokemon.name}.png`"
+        :alt="pokemon.name"
+        class="img-fluid"
+        style="width: 64px; height: 64px"
+      />
       <div>
-        <span class="text-muted small">#001</span>
-        <p class="mb-0 fw-semibold text-capitalize">nome</p>
+        <span class="text-muted small">#{{ pokemon.key }}</span>
+        <p class="mb-0 fw-semibold text-capitalize">{{ pokemon.name }}</p>
       </div>
     </div>
 
     <div>
-      <span class="badge bg-primary text-capitalize">type</span>
+      <span class="badge bg-primary text-capitalize"></span>
     </div>
 
-    <button class="btn btn-outline-primary ms-3">more</button>
+    <a class="btn btn-outline-primary ms-3">more</a>
   </div>
 </template>
