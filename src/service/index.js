7a42de5220ba31dae81fe02ff23d95db055edfb9
Souza <bossviti064@gmail.com>
2025-04-08 21:07:18 -0300

    ✨ feat: add async function to fetch Pokémon details

--- src/service/index.js
+++ src/service/index.js
@@ -12,6 +12,17 @@ class requestdata {
       console.error(err);
     }
   }
+  async fetchReposityDetalis(id) {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      const data = response.data;
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 export default requestdata;
