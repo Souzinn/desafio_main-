edd1e5f6ed1181732a5df3383e89b263ce8190b2
Souza <bossviti064@gmail.com>
2025-04-10 09:43:54 -0300

    ✨ feat: add async function to fetch Pokémon evolution

--- src/service/index.js
+++ src/service/index.js
@@ -23,6 +23,26 @@ class requestdata {
       console.error(err);
     }
   }
+  async fetchEvolutionChain(id) {
+    try {
+      const speciesRes = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon-species/${id}`
+      );
+      const evoUrl = speciesRes.data.evolution_chain.url;
+      const evoRes = await axios.get(evoUrl);
+
+      const chain = [];
+      let current = evoRes.data.chain;
+      while (current) {
+        chain.push(current.species.name);
+        current = current.evolves_to[0];
+      }
+
+      return chain;
+    } catch (err) {
+      console.error("Erro na evolução:", err);
+    }
+  }
 }
 
 export default requestdata;
