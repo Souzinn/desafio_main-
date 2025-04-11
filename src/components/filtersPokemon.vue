ba84bc8af51f809ef42f4e2dd75d40c6ecba6a33
Souza <bossviti064@gmail.com>
2025-04-10 22:48:44 -0300

    ♻️ refactor:  new components and improvements to filtering

---
+++ src/components/filtersPokemon.vue
@@ -0,0 +1,36 @@
+<script setup>
+import inputSearch from "../assets/abstractions/input.vue";
+import inputName from "../assets/abstractions/inputName.vue";
+import inputId from "../assets/abstractions/inputId.vue";
+
+defineProps([
+  "updateSearchName",
+  "updateSelectedType",
+  "updateSearchId",
+  "searchId",
+  "searchName",
+  "selectedType",
+]);
+</script>
+
+<template>
+  <div class="container mb-4">
+    <div class="row g-3 align-items-center">
+      <div class="col-md">
+        <inputName
+          v-if="!searchId && !selectedType"
+          @input="updateSearchName"
+        />
+      </div>
+      <div class="col-md">
+        <inputSearch
+          v-if="!searchId && !searchName"
+          @input="updateSelectedType"
+        />
+      </div>
+      <div class="col-md">
+        <inputId v-if="!searchName && !selectedType" @input="updateSearchId" />
+      </div>
+    </div>
+  </div>
+</template>
