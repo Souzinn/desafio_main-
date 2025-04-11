7edb3c64cdef91f1d830db931f89f95367d9380f
Souza <bossviti064@gmail.com>
2025-04-10 22:03:56 -0300

    ✨ feat: improve Pokémon search functions with structured feedback

--- src/service/index.js
+++ src/service/index.js
@@ -1,28 +1,46 @@
 import axios from "axios";
 
 class requestdata {
+  //busca a lista de Pokémon - nome e ID
   async fetchReposity(page) {
     try {
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=30`
       );
-      const data = response.data;
-      return data;
+      return response.data.results.map((pokemon, index) => ({
+        id: page + index + 1,
+        name: pokemon.name,
+      }));
     } catch (err) {
-      console.error(err);
+      console.error("Erro ao buscar lista de Pokémon:", err);
     }
   }
+
+  //buscar detalhes Pokémon
   async fetchReposityDetalis(id) {
     try {
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${id}`
       );
       const data = response.data;
-      return data;
+
+      return {
+        id: data.id,
+        name: data.name,
+        types: data.types.map((type) => type.type.name),
+        moves: data.moves.slice(0, 5).map((move) => move.move.name),
+        sprites: {
+          front: data.sprites.front_default,
+          shinyFront: data.sprites.front_shiny,
+        },
+        games: data.game_indices.map((game) => game.version.name),
+      };
     } catch (err) {
-      console.error(err);
+      console.error("Erro ao buscar detalhes do Pokémon:", err);
     }
   }
+
+  //busca evolução
   async fetchEvolutionChain(id) {
     try {
       const speciesRes = await axios.get(
@@ -40,7 +58,7 @@ class requestdata {
 
       return chain;
     } catch (err) {
-      console.error("Erro na evolução:", err);
+      console.error("Erro ao buscar cadeia de evolução:", err);
     }
   }
 }
