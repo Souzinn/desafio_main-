767102cb49ef6fea652ab203c138ab44217d5705
Souza <bossviti064@gmail.com>
2025-04-10 22:12:04 -0300

    🚚 chore: move input components to abstraction folder

---
+++ src/assets/abstractions/inputName.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  placeholder: String,
+});
+
+const emit = defineEmits(["input"]);
+</script>
+
+<template>
+  <input
+    :placeholder="placeholder || 'Filtrar por nome...'"
+    class="form-control"
+    type="text"
+    @input="(e) => emit('input', e.target.value.trim())"
+  />
+</template>
